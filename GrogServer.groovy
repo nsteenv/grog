5622c57f82e1b5ac234672825aa2fa3d4d4e9a70
v-vansteen <v.vansteenbergen@gmail.com>
2014-03-30 18:09:48 +0200

    Change assets layout + add jetty preview

--- GrogServer.groovy
+++ GrogServer.groovy
@@ -1,28 +1,13 @@
 @Grab('org.mortbay.jetty:jetty-embedded:6.1.26')
-import groovy.servlet.GroovyServlet
-import groovy.servlet.TemplateServlet
-
-@Grab('org.mortbay.jetty:jetty-embedded:6.1.26')
-import static org.mortbay.jetty.Handler.DEFAULT
 import org.mortbay.jetty.Server
 import org.mortbay.jetty.servlet.Context
 import org.mortbay.jetty.servlet.DefaultServlet
-import org.mortbay.servlet.MultiPartFilter
 
 new Server(8080).with {
     new Context(it, '/', Context.SESSIONS).with {
-        resourceBase = '.'
-        addFilter MultiPartFilter, '/*', DEFAULT
-        addServlet(DefaultServlet, '/static/*').with {
+        resourceBase = './build/grog/'
+        addServlet(DefaultServlet, '/*').with {
             setInitParameter 'dirAllowed', 'true'
-        }
-        addServlet(TemplateServlet, '/view/*').with {
-            setInitParameter 'resource.name.regex', '/view(.*)'
-            setInitParameter 'resource.name.replacement', 'gsp/$1.gsp'
-        }
-        addServlet(GroovyServlet, '/groovy/*').with {
-            setInitParameter 'resource.name.regex', '/groovy(.*)'
-            setInitParameter 'resource.name.replacement', 'groovy/$1.groovy'
         }
     }
     start()
